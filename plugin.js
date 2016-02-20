b418cbf8562d3d31cf440a8922fbfe393d56de0d
Alex Nazaruk <flexage@gmail.com>
2016-02-20 13:17:37 +0000

    Updated: Backmenu with @LukePulverenti 's changes

--- plugin.js
+++ plugin.js
@@ -288,21 +288,23 @@ define(['playbackManager', 'pluginManager'], function (playbackManager, pluginMa
 
         self.showUserMenu = function () {
 
-            // For now just go cheap
-            showBackMenuInternal(function () { }, true);
+            // For now just go cheap and re-use the back menu
+            showBackMenuInternal(true);
         };
 
-        self.showBackMenu = function (callback) {
+        self.showBackMenu = function () {
 
-            showBackMenuInternal(callback, false);
+            return showBackMenuInternal(false);
         };
 
-        function showBackMenuInternal(callback, showHome) {
+        function showBackMenuInternal(showHome) {
 
-            require([pluginManager.mapPath(self, 'backmenu/backmenu.js')], function (showBackMenu) {
-                showBackMenu({
-                    callback: callback,
-                    showHome: showHome
+            return new Promise(function (resolve, reject) {
+
+                require(['backMenu'], function (showBackMenu) {
+                    showBackMenu({
+                        showHome: showHome
+                    }).then(resolve);
                 });
             });
         }
